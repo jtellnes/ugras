c27cb457cf0b5feb55ac9424dd07a896b573f626
Jørgen Tellnes <jorgen@tellnes.nu>
2016-09-06 21:07:14 +0200

    Add site files

---
+++ js/ugras-resources.js
@@ -0,0 +1,93 @@
+
+var ugrasConcerts = [
+	/* *** Kan offentleggjerast 01.01.17 ***
+	{ date: '2017-05-26', name: 'Spydeberg Rock Festival (26.05 eller 27.05)', info: 'Ugras spelar på Spydeberg Rock Festival',
+	links: [
+			 { title: 'Spydeberg Rock Festival', url: "http://spydebergrock.no/" },
+		], },
+	*/
+	{ date: '2016-08-20', name: 'Telemarkfestivalen, Sandvin', info: 'Ugras spelar på laurdagen, før Hayseed Dixie. Parkscenen' },
+	{ date: '2016-08-19', name: 'Telemarkfestivalen, Pizzafjoset', info: 'Folkemusikkpub med dans på Pizzafjoset' },
+	{ date: '2016-08-19', name: 'Telemarkfestivalen, Bøsenteret', info: 'Kl. 12.00. Ugras spelar nokre låtar på Bøsenteret' },
+	{ date: '2016-07-08', name: 'Nattåpent, Bøsenteret' },
+	{ date: '2016-06-23', name: 'Heime hos Mosfjeld', info: 'Ugras spelar St.Hans-konsert heime hos Mosfjell', links: [{ title: 'Sjå Facebook-event', url: 'https://www.facebook.com/events/1047237575368928/'}] },
+	{ date: '2016-05-28', name: 'Teledølen, Notodden' },
+	{ date: '2016-05-26', name: 'Telemarkscenen, radiosending', info: 'Ugras spelar på Telemarksendinga (NRK P1), direkte, kl. 15.05',
+		links: [
+			 { title: 'Høyr heile radioinnslaget med intervju (Klikk berre direkte på play i NRK-spelaren, så kjem innslaget frå start)', url: "https://radio.nrk.no/serie/distriktsprogram-telemark/DKTE02009816/26-05-2016#t=1h2m17s" },
+		],
+        videos: [
+		     { title: "Aller meir", embeddedsrc: "https://www.youtube.com/embed/OPsBWcLy_zw" },
+		     { title: "Hikkefull mann", embeddedsrc: "https://www.youtube.com/embed/FbgKrj1fuS8" },
+		     { title: "Lortebekk", embeddedsrc: "https://www.youtube.com/embed/BcXKy64em7I" }
+		]
+    },
+	{ date: '2016-01-29', name: 'Oppvarming for NiLiv på Meieriet i Lunde', info: 'Ugras er oppvarmingsband for NiLiv på Meieriet i Lunde',
+	  /*
+	  videos: [
+	  	  { title: 'Audun med felesolo på Slepp meg ut', embeddedsrc: '<div id="fb-root"></div><script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/nn_NO/sdk.js#xfbml=1&version=v2.3";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script><div class="fb-video" data-allowfullscreen="1" data-href="/ugrasband/videos/vb.477599282334405/998148676946127/?type=3"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/ugrasband/videos/998148676946127/"><a href="https://www.facebook.com/ugrasband/videos/998148676946127/">Slepp meg ut</a><p>Audun Rørmark med felesolo under premiera på Ugras&#039; nye låt &quot;Slepp meg ut&quot;, skrive av Stian Dalen</p>Posted by <a href="https://www.facebook.com/ugrasband/">Ugras</a> on 2. februar 2016</blockquote></div></div>' }
+	  	  { title: 'Audun med felesolo på Slepp meg ut', embeddedsrc: "https://www.facebook.com/ugrasband/videos/vb.477599282334405/998148676946127/?type=3" }
+	  ],
+	  */
+	  links: [
+	  		{ title: 'Audun med felesolo på Slepp meg ut', url: "https://www.facebook.com/ugrasband/videos/998148676946127/" },
+	  		{ title: "NiLivs nettstad", url: "http://niliv.no" }
+	  	]
+	},
+	{ date: '2015-12-08', name: 'Prisutdeling på Gulltrimmen', info: 'Ugras spelar eit par låtar under prisutdelinga på Gulltrimmen' },
+	{
+  	  date: '2015-09-18', name: 'Åpning av Nrk Telemarks lokalkontor i Bø', info: 'Info om Nrk-konserten...',
+	  images: [ 
+	    { title: "Ugras", src: "images/concerts/nrktilboe/Ugras_Nrk_Telemark_18.09.2015.jpg", photographer: "Nrk" },
+	  ],
+	  links: [
+		{ title: "Oppslag på nrk.no", url: "http://www.nrk.no/telemark/her-apner-ordforeren-i-bo-det-nye-nrk-kontoret-1.12560351" },
+		{ title: "Radiosendinga på nrk.no", url: "https://radio.nrk.no/serie/distriktsprogram-telemark/DKTE02018515/18-09-2015#t=4m3s" }
+	  ]
+	},
+	{ date: '2015-08-04', name: 'Kartfestivalen 2015', info: 'Ugras er Kartfestivalens "husband" på fredagen',
+	  links: [
+		{ title: "Bøbladvideo fra før konserten", url: "https://www.facebook.com/1390550951251592/videos/vb.1390550951251592/1452471415059545/?type=2&theater" }
+	  ]
+	},
+    { date: '2015-06-18', name: 'Lortebekk Hillbilly Music Fest 2015' },
+	{ date: '2015-01-03', name: 'Nyårskonsert på Bull Inn' },
+	{ date: '2014-10-25', name: 'Gleden Scene på Ulefoss' },
+	{ date: '2014-10-18', name: 'Privat arrangement, Skien' },
+	{ date: '2014-08-31', name: 'Privat arrangement, Oslo' },
+	{ date: '2014-08-16', name: 'Kartfestivalen 2014', info: 'Ugras held åpningskonserten på laurdagen',
+		images: [ 
+		// { title: "Ugras", src: "images/concerts/kartfestival_2014/noko.jpg", photographer: "Nokon" }			    	
+		],
+		videos: [
+			{ title: "Hønemor", embeddedsrc: "https://www.youtube.com/embed/bVLYjRWOgXI" },
+			{ title: "Ugras", embeddedsrc: "https://www.youtube.com/embed/qwPux7c4-is" }
+		],
+	},
+	{ date: '2014-08-07', name: 'Telemarkfestivalen 2014' },
+	{ date: '2014-07-11', name: 'Utekonsert ved Bøsenteret' },
+	{ date: '2014-06-28', name: 'Sønstebøtunet' },
+	{ date: '2014-06-13', name: 'Lortebekk Hillbilly Music Fest 2014' },
+	{ date: '2014-06-01', name: 'Utekonsert på Kvennøya' },
+	{ date: '2013-12-27', name: 'Konsert på Bull Inn' },
+	{ date: '2013-12-05', name: 'Privat arrangement, Lifjell' },
+	{ date: '2013-09-28', name: 'Bygdefest, Norsjø ferieland' },
+	{ date: '2013-09-21', name: 'Privat arrangement, Bø' },
+	{ date: '2013-08-08', name: 'Telemarkfestivalen 2013' },
+	{ date: '2013-08-03', name: 'Formiddagskonsert på Sønstebøtunet' },
+	{ date: '2014-09-19', name: 'Privat arrangement, Eikjabygda' },
+	{ date: '2012-12-27', name: 'Romjulskonsert, Bull Inn' },
+];
+
+var ugrasSongs = [
+	{ title: 'Hønemor', lyricsBy: 'Stian Dalen', musicBy: 'Trad. / Stian Dalen', soundCloudId: 231569586, trackName: 'honemor', audiofile: '01_Hoenemor.mp3', lyrics: "\nEi gammal høne e elt eg eig\nho klukkær ennå, e rimleg seig\nHo legg da egg, men det e få\nså eg lurær på å høgg'a ho hue nå\n\nHønemor, hønemor\nha'kje lagt egg sea høsten i fjor\nLegg egg nå så ska'ru sjå\nat du fær behelde hue ditt på\n\nDen gamle høna e passeleg sær\nvi'kje veta av meg der ho subbær rondt her\nnår eg skal sjå om ho har lagt,\nhøgg ho te med nebb og forakt\n\nHønemor, hønemor\nha'kje lagt egg sea høsten i fjor\nLegg egg nå så ska'ru sjå\nat du fær behelde hue ditt på\n\nMe kjem neiggu lenger overens\nLegg egg nå, eg e pengelens\nSå kan ho ikkje gje meg det eg treng\ne det ikkje lenge før ho heng og sleng\n\nHønemor, hønemor\nha'kje lagt egg sea høsten i fjor\nLegg egg nå så ska'ru sjå\nat du fær behelde hue ditt på\n\nNå legg ho egg som ingen aen\nJa, eg klekka ut ein plan\nEg sa: Du kan sjølv tenke deg faren\nom eg slepp reven inn i garen\n\nHønemor, hønemor\nha'kje lagt egg sea høsten i fjor\nLegg egg nå så ska'ru sjå\nat du fær behelde hue ditt på\n\nHønemor, hønemor\nha'kje lagt egg sea høsten i fjor\nLegg egg nå så ska'ru sjå\nat du fær behelde hue ditt på" },
+	{ title: 'Ugras', lyricsBy: 'Stian Dalen', musicBy: 'Stian Dalen', soundCloudId: 231569584, trackName: 'ugras', audiofile: '02_Ugras.mp3', lyrics: "\nHeime på jorda der veks det bare ugras\nSå det å væra bonde, det e'kje stort stas\n\nEg e ein bonde utan kyr\nstend opp langt fyre dagen gryr\nJordet e blitt te ei hengemyr\nEg e så lei, eg trur eg spyr\n\nSøknad her og stønad der\nDom lovær gull og grøne skogær\nLa meg heller få jordet regulert\nte eit bustadfelt, e du interessert?\n\nPå jordet veks det bare ugras\nÅ væra bonde e'kje stort stas\nSå kønn vil eg slutte med\nSå nå sett eg meg ned og lar jordet gro te\n\nEg va ein gang ein entusiast\nTok over garden i ei hast\nStore planer om å jobbe iherdig\npå ein gard som knapt nok e driveverdig\n\nKonne tenkt meg å jobba på kontor\ni toppen av ein bygning som va stor\nTikje farvel med traktor'n og geita\nog fyki på møter i hutti heita\n\nPå jordet veks det bare ugras\nÅ væra bonde e'kje stort stas\nSå kønn vil eg slutte med\nSå nå sett eg meg ned og lar jordet gro te\n\nUgras, ugras, ugras, ugras\n\nEg e ein bonde utan kyr\nstend opp langt fyre dagen gryr\nJordet e blitt te ei hengemyr\nEg e så lei, eg trur eg spyr\n\nPå jordet veks det bare ugras\nÅ væra bonde e'kje stort stas\nSå kønn vil eg slutte med\nSå nå sett eg meg te og lar sola gå ned" },
+	{ title: 'Dansesko', lyricsBy: 'Stian Dalen', musicBy: 'Stian Dalen', soundCloudId: 231569579, trackName: 'dansesko', audiofile: '03_Dansesko.mp3', lyrics: "\nMed ein liten dram og fela i eit skrin\nDu ser'n kjem ruslans der nedpå stien\nDet e ein spelemann som e på veg\nOg du kan sjå håss han gledar seg\n\nTa på deg dansesko\nTa på deg dansesko\nStemninga e god, det her blir moro\nTa på deg dansesko\n\nKom her, her blir det spell\nMe dreg i gang her nå mens dagen e på hell\nMe ladar opp, slepp det ikkje ned\nfør heile nåtta e omme\n\nTa på deg dansesko...\n\nNå e'n i form og godt driv\nKjem som eit freskt pust av ny giv\nIngen ende vil det ta\nNei, denna festen tek heilt av\n\nTa på deg dansesko..." },
+	{ title: 'Hikkefull mann', lyricsBy: 'Jon Solberg', musicBy: 'Trad.', soundCloudId: 231569574, trackName: 'hikkefull_mann', audiofile: '04_Hikkefull_mann.mp3', lyrics: "\nEg har spela kort i England, eg har gambla i USA\nEg har festa og tura slik ein bøhering ska\nTexas Hold'em, Texas Hold'em, du har gjort meg te fant\nLønna gjekk i potten og fela i pant\n\nWhiskey og fine damer vil bli min undergang\nEg har prøva å slutte, men det gjeng ikkje an\nHan fyren borti baren e pokker så treg\nmeir brennvin, meir brennvin, meir brennvin te meg\n\nEg vaknar utpå dagen, bakfull og blakk\nForbanna Texas Hold'em og ell whiskey'n eg drakk\nTexas Hold'em, Texas Hold'em, du har gjort meg te fant\nLønna gjekk i potten og fela i pant\n\nViss elva var whiskey og eg var ei and\nsku' eg dykka ned te bånn og aller svømt inn te land\nMen elva e'kje whisky og eg e'kje ei and\nEg e bare ein lutfattig hikkefull mann\n\nHikk, hikk, hikk..." },
+	{ title: 'Anne Fagerli', lyricsBy: 'Jon Solberg', musicBy: 'Trad.', soundCloudId: 231569570, trackName: 'anne_fagerli', audiofile: '05_Anne_Fagerli.mp3', lyrics: "\nI lia ligg eit hus, opp til huset gjeng ein veg\nHått ska ein stakkar gjera?\nI huset bur ei jente som ikkje vil ha meg\nAnne e best i verda\n\nEg ser på når ho smiler, eg ser på når ho ler\nHått ska ein stakkar gjera?\nAv gutan' er det bare meg ho ikkje ser\nAnne e best i verda\n\nAnne, Anne Fagerli\nTa meg med i dansen\nAnne, Anne Fagerli\nKjære Anne, gje meg sjansen\n\nOg når ho gjeng forbi stoppar elle for å sjå\nHått ska ein stakkar gjera?\nMannfølka blir raude og kvinnfølka blir grå\nAnne e best i verda\n\nSola lyser gul og månen lyser gvit\nHått ska ein stakkar gjera?\nAnne lyser regnbågens fargar like hit\nAnne e best i verda\n\nAnne, Anne Fagerli...\n\nI lia ligg eit hus, opp til huset gjeng ein veg\nI huset bur ei jente som ikkje vil ha meg\nPå huset e ei dynn og på dynna bankar eg\nAnne, slepp meg inn, eg kan'kje leva utta deg\n\nAnne, Anne Fagerli..." },
+	{ title: 'Lortebekk', lyricsBy: 'Jon Solberg', musicBy: 'Trad.', soundCloudId: 231569567, trackName: 'lortebekk', audiofile: '06_Lortebekk.mp3', lyrics: "\nBlod med loppur og haga med makk\nhaga med makk, haga med makk\nhopp i skoa og fort deg og pakk\nMe skal te Lortebekk og fiske\n\nEi bytte med makk blir ei bytte med fisk\nei bytte med fisk, ei bytte med fisk\nDu fæ'kje kjøpt bære fisk over disk\nMe skal te Lortebekk og fiske\n\nLomme med lerke og påså med pils\npåså med pils, påså med pils\npå benken sit Biggen og Nåså-Nils\nMe skal te Lortebekk og fiske\n\nLigge og lure og vente på napp\nvente på napp, vente på napp\nog når'n tek'n ja da må du vera kjapp\nMe skal te Lortebekk og fiske\n\nLortebekk, Lortebekk, Lortebekk hoy!\n\nRegnet gjer meg våt, sola gjer meg tørr\nsola gjer meg tørr, sola gjer meg tørr\nBålbaka aure med rjome og smør\nMe skal te Lortebekk og fiske\n\nStonga i veret og snøret i bånn\nsnøret i bånn, snøret i bånn\nLigge og drøyme om Guri på Strånd\nMe skal te Lortebekk og fiske\n\nLortebekk, Lortebekk, Lortebekk hoy!" 	},
+	{ title: 'Den svarte kråka', lyricsBy: 'Omsett av Jon Solberg', musicBy: 'Trad.', soundCloudId: 231569562, trackName: 'den_svarte_kraaka', audiofile: '07_Den_svarte_kraaka.mp3', lyrics: "\nNår tida renn min kjære venn og me må ta farvel\nÅ, om du bare konne sjå så tungt eit hjerte fell\nnår mørket kryp inn i krik og krok og dagen e forbi\nsku ønske eg konn vera med, hell at du konne bli\n\nDen svarte kråka blir så gvit som sno og eg veit godt\nat om eg ein gong sku svike deg vil høglys dag bli nått\nVil høglys dag bli nått, min venn, da gret himmel og jord\nOm eg ein gong sku svike deg, da rasar fjell i fjord\n\nOg når du stødig stig i land der elt e frodig og grønt\nNår vinden bles og havet syng så sender eg ein bønn\nNår vinden bles og havet syng, send kjærleik hit te meg\nså eg kan kjenne varmen din langs stig og landeveg" },
+	{ title: 'Fridag', lyricsBy: 'Jørgen Tellnes', musicBy: 'Jørgen Tellnes', soundCloudId: 231569558, trackName: 'fridag', audiofile: '08_Fridag.mp3', lyrics: "\nDet e fri i dag og himmelen e blå\nog tjugetri grader, solbrillun e på\nog nedved elvestryka setter eg meg te\nElle bekymringer reiser vatnet med\n\nDet e fri i dag og himmelen e grå\neg e i skogen med regnjakka på\nI freske vinddrag med lukt av furutre\nfær kroppen pådrag og sjela ro og fred\n\nFridag, ein «væra ut'i det det fri»-dag\nBlid dag, ein «bære kan det'kje bli»-dag\n\nDet e ein kald dag og bles som berre det\nog det e torever og regnet høljer ned\nmen det e lunt her med ømmen fyra opp\nog snart kjem du innom på ein kaffikopp\n\nFridag, ein «ned på knea og fri»-dag\nBlid dag, ein «bære kan det'kje bli»-dag" },
+	{ title: 'Tjyrublomen', lyricsBy: '', musicBy: 'Jon Solberg', soundCloudId: 231569555, trackName: 'tjyrublomen', audiofile: '09_Tjyrublomen.mp3', lyrics: "" },
+	{ title: 'Heim te Bø', lyricsBy: 'Stian Dalen', musicBy: 'Stian Dalen', soundCloudId: 231569551, trackName: 'heim_te_boe', audiofile: '10_Heim_te_Boe.mp3', lyrics: "\nEg e'kje akkurat verdensvant\neg lyg'kje når eg pratar sant\nSå sier eg det at verden e stor\nEg vil ut ein tur på moder jord\n\nLitt smør og brød eg hadde med\nnår eg la ut på det ukjente\nEg ville bli vis, eg ville bli lerd\nKome heimatt som gutten etter jordomferd\n\nHeim, eg lengtar heim, eg lengtar så heim\nEg lengtar heimatt te Bø\nEg tregar så på at eg reste\nEg lengtar heimatt te Bø\n\nBlei fort heimsjuk da eg møtte på\nfølk eg ikkje kunne forstå\n(som) bruka fremmedord og prata rart\nEg måtte kome meg heimatt snart\n\nTrudde livet sku' bli eit fengans refreng\nog at graset blei grønt som i ei blomstereng\nmen den gong ei og den gong da\nla eg halen mellom beina\n\nHeim, eg lengtar heim...\n\nEg e'kje akkurat verdensvant\neg lyg'kje når eg pratar sant\nSå sier eg det at verden e stor\nEg vil ut ein tur på moder jord\n\nHeim, eg lengtar heim..." }
+];
---
+++ js/ugras.js
@@ -0,0 +1,294 @@
+
+Vue.component('concerts', {
+
+    template: '#concerts',
+
+    data: function() {
+        return {
+            concerts: null,
+            nextConcert: null,
+            listIsLimited: true,
+            dateTest: new Date()
+        }
+    },
+    computed: {
+        concertList: function() {
+            return this.listIsLimited ? this.concerts.slice(this.concerts.indexOf(this.nextConcert), this.concerts.length) : this.concerts;
+        },
+    },
+    created: function() {
+        this.concerts = ugrasConcerts.sort(function(a, b) {
+            return new Date(a.date).getTime() - new Date(b.date).getTime();
+        });
+        
+        // Depends on compability.js on some plattforms
+        this.nextConcert = this.concerts.concat().find(function(concert) {
+            return new Date(concert.date) > new Date().setHours(0,0,0,0);
+        });
+    },
+    methods: {
+        limitList: function() {
+            this.listIsLimited = true;
+        },
+        unlimitList: function() {
+            this.listIsLimited = false;
+        },
+        timeToConcert: function(concert) {
+            var concertDate = new Date(concert.date).setHours(0,0,0,0);
+            var toDaysDate = new Date().setHours(0,0,0,0);
+            return Math.round((concertDate - toDaysDate) / 86400000); // Days
+        },
+        timeSinceConcert: function(concert) {
+            var concertDate = new Date(concert.date).setHours(0,0,0,0);
+            var toDaysDate = new Date().setHours(0,0,0,0);
+            return Math.round((toDaysDate - concertDate) / 86400000); // Days
+        },
+        timeToNextConcert: function() {
+            return this.timeToConcert(this.nextConcert);
+        }
+    },
+    components: {
+        concert: {
+            template: '#concert',
+            props: ['concert'],
+            data: function() {
+                return {
+                    showMore: false
+                }
+            },
+            computed: {
+                formattedDate: function() {
+                    var date = new Date(this.concert.date);
+                    return this.$root.formatDate(date);
+                },
+                rowStyle: function() {
+                    var classes = this.isPast() ? 'past' : 'future';
+                    if(!this.showMore) classes += ' end';
+                    return classes;
+                }
+            },
+            methods: {
+                timeToConcert: function() {
+                    return this.$parent.timeToConcert(this.concert);
+                },
+                timeSinceConcert: function() {
+                    return this.$parent.timeSinceConcert(this.concert);
+                },
+                isPast: function() {
+                    return new Date(this.concert.date) < new Date().setHours(0,0,0,0);
+                },
+                isNext: function() {
+                    return this.concert == this.$parent.nextConcert;
+                },
+                toggleShowMore: function() {
+                    if(this.concert.info)
+                        this.showMore = !this.showMore;
+                },
+
+            }
+        }
+    }
+});
+
+Vue.component('music', {
+
+    template: '#music',
+    
+    data:  function() {
+        return {
+            songs: ugrasSongs,
+            loadedSong: '',
+            player: '',
+            playerSong: '',
+        }
+    },
+    created: function() {
+        this.playerSong = this.songs[0];
+      //  this.loadPlayer(this.playerSong);
+    },
+    computed: {
+        songTitle: function() {
+            return this.loadedSong.title;
+        },
+        musicBy: function() {
+            return this.loadedSong.musicBy;
+        },
+        lyricsBy: function() {
+            return this.loadedSong.lyricsBy;
+        },
+        lyrics: function() {
+            return this.loadedSong.lyrics;
+        }
+    },
+    methods: {
+        loadSong: function(song) {
+            this.loadedSong = song;
+            this.loadPlayer(song);
+            this.playerSong = song;
+        },
+        isLoaded: function(song) {
+            return this.loadedSong == song;
+        },
+    },
+    components: {
+        'song': {
+            template: '#song',
+            props: ['song'],
+            
+            data: function() {
+                return {
+                    showMore: false
+                }
+            },
+            methods: {
+                startInPlayer: function(song) {
+                    this.$dispatch('songWantedPlayed', song);
+                },
+                toggleShowMore: function() {
+                    this.showMore = !this.showMore;
+                }
+            }
+        }
+    }
+});
+
+Vue.component('musicplayer', {
+
+    template: '#musicplayer',
+    //props: ['song'],
+    data: function() {
+        return {
+            songs: ugrasSongs,
+            currentSong: null,
+            player: null
+        }
+    },
+    created: function() {
+
+        if(this.$root.scIsAccessable()) {
+            SC.initialize({ client_id: '5e772b572fc1d3481f55dfba503cd4bc' });
+            this.load(this.songs[0]);
+        }
+    },
+    computed: {
+        currentTrackNumber: function() {
+            return this.songs.indexOf(this.currentSong) + 1;
+        }
+    },
+    methods: {
+        load: function(song) {
+            
+            this.currentSong = song;
+            
+            if(this.player) {
+                this.player.pause();
+                this.player.seek(0);
+            }
+            
+            var streamPath = '/tracks/' + this.currentSong.soundCloudId;
+            return SC.stream(streamPath).then(function(player) {
+                this.player = player;
+                this.player.on('finish', function() {
+                    this.next();
+                }.bind(this));
+            }.bind(this));
+        },
+        next: function() {
+            // Load the first song if user skips ahead from the last song:
+            var nextTrackNumber = (this.currentTrackNumber == this.songs.length) ? 1 : this.currentTrackNumber + 1;
+            songToLoad = this.songs[nextTrackNumber-1];
+            this.load(songToLoad).then(function() {
+                this.player.play();
+            }.bind(this));
+
+        },
+        previous: function() {
+            var previousTrackNumber;
+            if(this.player.currentTime() > 1000) {
+                this.player.seek(0);
+                return
+            }
+            // Load the last song if user skips back from the first song:
+            var previousTrackNumber = (this.currentTrackNumber == 1) ? this.songs.length : this.currentTrackNumber - 1;
+            songToLoad = this.songs[previousTrackNumber-1];
+            this.load(songToLoad).then(function() {
+                this.player.play();
+            }.bind(this));
+        },
+        forward: function() {
+            this.player.seek(this.player.currentTime() + 15000);
+        },
+        backward: function(){
+            this.player.seek(this.player.currentTime() - 5000);
+        }
+    },
+    events: {
+        'playSong': function(song) {
+            this.load(song).then(function() {
+                this.player.play();
+            }.bind(this));
+        }
+    }
+});
+
+Vue.component('about', {
+
+    template: '#about',
+
+});
+
+Vue.component('contact', {
+
+    template: '#contact',
+    
+    data: function() {
+      return { showAll: false }
+  }
+});
+
+Vue.component('bottominfo', {
+
+    template: '#bottominfo',
+    methods: {
+        getCurrentYear:  function() {
+            return new Date().getFullYear();
+        }
+    },
+});
+
+Vue.component('totoplink', {
+
+    template: '#totoplink'
+});
+
+new Vue({
+
+	el: 'body',
+
+    methods: {
+        formatDate: function(d) {
+
+            var dd = d.getDate();
+            if ( dd < 10 )
+                dd = '0' + dd;
+
+            var mm = d.getMonth()+1;
+            if ( mm < 10 )
+                mm = '0' + mm;
+
+            var yy = d.getFullYear() % 100;
+            if ( yy < 10 )
+                yy = '0' + yy;
+
+            return dd+'.'+mm+'.'+yy;
+        },
+        scIsAccessable: function() {
+            return typeof SC !== 'undefined' && SC !== null;
+        }
+    },
+    events: {
+        'songWantedPlayed': function(song) {
+           this.$broadcast('playSong', song);
+        }
+    }
+});
